5bf714f9ae0c6d02f07e035e171d5610e8f9ca4d
vikramsingh13 <vikramsingh.webdev@gmail.com>
2022-08-08 03:04:49 -0400

    backend refactoring, setting up front end client

--- server/index.js
+++ server/index.js
@@ -1,31 +1,32 @@
-const express = require('express');
 const mongoose = require('mongoose');
+const express = require('express');
+const Task = require('./models/Task');
+
 const app = express();
-const TaskModel = require('./models/Task.js');
-
-
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://admin:LetMeMongo1337@cluster0.p0md3ko.mongodb.net/tasksData?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    });
-
-app.get('/', async(req, res) => {
-    const task = new TaskModel({taskName: "task3", 
-    taskDesc:"test task",
-    taskDueDate:"tomorrow",
-    });
-
-    try{
-        await task.save();
-        console.log("Task saved.");
-    } catch(err){
-        console.warn("Error: ", err);
+mongoose.connect(
+    "mongodb+srv://admin:LetMeMongo1337@cluster0.p0md3ko.mongodb.net/tasksData?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
     }
-})
-
-app.listen(8000, () => {
-    console.log("Server running on port 8000!");
+).catch(err => {
+    console.warn("Connection Error: ", err);
+}).then(() => {
+    console.log("Connection to MongoDB atlas was successful");
 });
 
+app.post('/insert', (req, res) => {
+    const task = new Task({taskName: "todo"});
+    try{
+        task.save();
+        console.log("Task saved. ");
+    }catch(err){
+        console.warn("Error saving: ", err);
+    }
+});
+
+app.listen(3001, '0.0.0.0', 50, ()=> {
+    console.log("Express server listening on port: 3001 / 0.0.0.0");
+});
 
--- server/models/Task.js
+++ server/models/Task.js
@@ -3,17 +3,10 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
     taskName: {
         type: String,
-        requried: true,
-    },
-    taskDesc: {
-        type: String,
-        required: false,
-    },
-    taskDueDate: {
-        type: String,
-        required: false,
+        required: true
     }
 });
 
 const Task = mongoose.model("Task", TaskSchema);
+
 module.exports = Task;
