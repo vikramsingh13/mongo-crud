71eba3c9bd28911d5e520adee9c845e4c189061e
vikramsingh13 <vikramsingh.webdev@gmail.com>
2022-08-08 03:12:09 -0400

    insert tab added in front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="app-container">
+        <div className="app-heading">
+
+        </div>
+        <div className="app-insert-form">
+            <label className="app-label">Task Name:</label>
+            <input type="text" className="app-input" />
+            <button className="app-insert-button">Add Task</button>
+        </div>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const cors = require('cors');
 const Task = require('./models/Task');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(
     "mongodb+srv://admin:LetMeMongo1337@cluster0.p0md3ko.mongodb.net/tasksData?retryWrites=true&w=majority",
