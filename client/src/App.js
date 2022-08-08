c7199fcdbe4b9a1f0faf51f5cad4ce91ab9d14e9
vikramsingh13 <vikramsingh.webdev@gmail.com>
2022-08-08 03:51:43 -0400

    added axios and cors, tasks can be successfully created and added

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,46 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="app-container">
-        <div className="app-heading">
-            Task Manager
+    const [taskName, setTaskName] = useState('');
+
+    const addTask = async() => {
+        try{
+            await axios.post(
+                'http://localhost:3001/insert', 
+                {
+                    taskName: taskName,
+                }
+            )
+        } catch(err) {
+            console.warn("Axios post error: ", err);
+        }
+    };
+
+    return (
+        <div className="App">
+            <div className="app-container">
+                <div className="app-heading">
+                    Task Manager
+                </div>
+                <div className="app-insert-form">
+                    <label className="app-label">Task Name:</label>
+                    <input 
+                        type="text" 
+                        className="app-input"
+                        onChange={(event) => setTaskName(event.target.value) }
+                    />
+                    <button 
+                        className="app-insert-button"
+                        onClick={addTask}
+                    >
+                        Add Task
+                    </button>
+                </div>
+            </div>
         </div>
-        <div className="app-insert-form">
-            <label className="app-label">Task Name:</label>
-            <input type="text" className="app-input" />
-            <button className="app-insert-button">Add Task</button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
