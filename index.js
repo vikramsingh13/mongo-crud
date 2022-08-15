944135b427a4d9d1c52b0b569b918f58d264fd16
vikramsingh13 <vikramsingh.webdev@gmail.com>
2022-08-15 15:40:23 -0400

    error handler middleware added

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const path = require('path');
-const {errorHandler} = require('./middleware/errorMiddleware');
+const {errorHandler} = require('./middlewares/errorMiddleware');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: false}));
 
 app.use('/api/tasks', require("./routes/taskRoutes"));
 
---
+++ middlewares/errorMiddleware.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.status ? res.status : 500;
+
+    res.status(statusCode);
+    res.json({
+        Error: err.message,
+        stack: process.env.NODE_ENV == production ? null : err.stack,
+    });
+};
+
+module.exports = {
+    errorHandler
+};
+
