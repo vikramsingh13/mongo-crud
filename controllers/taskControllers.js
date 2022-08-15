af270e8719ac30b17564097d3756f6af4c64ccf2
vikramsingh13 <vikramsingh.webdev@gmail.com>
2022-08-15 15:49:27 -0400

    added express-async-handler for controllers

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -1,41 +1,41 @@
-
+const asyncHandler = require('express-async-handler');
 
 //@desc Get tasks
 //@path GET /api/tasks
 //access PRIVATE
-const getTasks = (req, res) => {
+const getTasks = asyncHandler(async (req, res) => {
 
     res.status(200).json({message: "Get tasks"});
-};
+});
 
 //@desc Set task
 //@path POST /api/tasks
 //access PRIVATE
-const setTask = (req, res) => {
+const setTask = asyncHandler(async (req, res) => {
     res.status(200).json({message: "Set task"});
-}
+});
 
 //@desc Update task
 //@path PUT /api/tasks
 //access PRIVATE
-const updateTask = (req, res) => {
+const updateTask = asyncHandler(async (req, res) => {
     if(!req.params.id){
         res.status(400);
         throw new Error('Please send a task id.');
     }
     res.status(200).json({message: `Update task : ${req.params.id}`});
-}
+});
 
 //@desc Delete tasks
 //@path DELETE /api/tasks
 //access PRIVATE
-const deleteTask = (req, res) => {
+const deleteTask = asyncHandler(async (req, res) => {
     if(!req.params.id){
         res.status(400);
         throw new Error('Please send a task id.');
     }
     res.status(200).json({message: `Delete task : ${req.params.id}`});
-}
+});
 
 module.exports = {
     getTasks,
